4ca26c14686185426952b546e602e10d2b56ba15
Chandan Sen <chandan2441139@gmail.com>
2021-09-26 21:32:13 +0600

    final edit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,12 +4,13 @@ const Header = () => {
     return (
         <div className="header">
             <div className="header-title">
-                 <h1>TOP 10 Billionaires Total <span>Assets</span></h1>
+                 <h1>Make a Social Organization With Top 10 Rechest People in the World.</h1>
             </div>
            <div className="header-content">
-           <p>The World's Billionaires is an annual ranking by documented net worth of the wealthiest billionaires in the world, compiled and published in March annually by the American business magazine Forbes. The list was first published in March 1987. Here we calculating their total asstes.</p>
+           <p>The World's Billionaires is an annual ranking by documented net worth of the wealthiest billionaires in the world, compiled and published in March annually by the American business magazine Forbes. The list was first published in March 1987. Here we calculating their total asstes to know how much money they have so that people get some motivation from them</p>
 
            </div>
+           <h2>Budget For the Organization: 150 Million Dollar</h2>
         </div>
     );
 };
